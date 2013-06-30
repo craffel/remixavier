b6b36d2570b127086eb9cf3105d4b88ad53cf6dd
Dan Ellis <dpwe@ee.columbia.edu>
2013-06-30 14:29:24 -0400

    weinerenhance now actually looks at its arguments

--- matlab/weinerenhance.m
+++ matlab/weinerenhance.m
@@ -1,15 +1,17 @@
-function y = weinerenhance(target, accomp, thresh, transit, fftlen)
-% y = weinerenhance(target, accomp, thresh, transit, fftlen)
+function [y,M,D] = weinerenhance(target, accomp, thresh, transit, fftlen)
+% [y,M,DTARG] = weinerenhance(target, accomp, thresh, transit, fftlen)
 %   Enhance a target by keeping only cells where it is large
 %   compared to an estimated accompaniment (e.g. the resid and targ
 %   from find_in_mix).
 %   thresh is the dB threshold (-6.0 by default) and transit is the
 %   sigmoid transition width (3.0).  fftlen is the length of the
 %   underlying fft (2048).
+%   M is the resulting mask, D is the unmodified corresponding STFT
+%   of target.
 % 2013-06-30 Dan Ellis dpwe@ee.columbia.edu
 
 if nargin < 3;  thresh = -6.0; end
-if nargin < 4;  tranist = 3.0; end
+if nargin < 4;  transit = 3.0; end
 if nargin < 5;  fftlen = 2048; end
 
 DTARG = specgram(target, fftlen);
@@ -21,4 +23,5 @@ DBRAT = 20*log10(abs(DTARG)) - 20*log10(abs(DCOMP));
 
 % Generate a mask by putting the dB difference through a sigmoid
 % with a midpoint at -6 dB, and a transition width ~ 3 dB
-y = ispecgram(DTARG .* sigmoid(DBRAT,-6,3));
+M = sigmoid(DBRAT,thresh,transit);
+y = ispecgram(DTARG .* M);
