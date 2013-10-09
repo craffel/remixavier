aed13a097a700d932661f43a6116a75cb485b6c9
Colin Raffel <craffel@gmail.com>
2013-10-09 14:16:20 -0400

    Files for downloading the dataset.

---
+++ Dataset/secret.py
@@ -0,0 +1,4 @@
+# Fill this information in before trying to access the what.cd API
+
+username="Your what.cd username"
+password="Your what.cd password"
---
+++ Dataset/whatapi/__init__.py
@@ -0,0 +1,2 @@
+from whatapi import WhatAPI
+__version__ = "0.1.1"
