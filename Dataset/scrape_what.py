e7b97dfe565a1fd43ec3b10b77b80d7e6eb69272
Colin Raffel <craffel@gmail.com>
2014-05-20 17:35:42 -0400

    Code for getting digital data

--- Dataset/scrape_what.py
+++ Dataset/scrape_what.py
@@ -131,3 +131,65 @@ def has_multiple_formats( group_ids, api_handle=None ):
     # Return all unique valid group IDs
     return np.unique(valid_group_ids).tolist()
 
+# <codecell>
+
+both = np.unique( np.loadtxt( 'valid_group_ids.txt' ) )
+for group_id in both:
+    print "https://what.cd/torrents.php?id={}".format( int(group_id) )
+
+# <codecell>
+
+def has_both_digital( group_ids, api_handle=None ):
+    '''
+    Given a list of group IDs, check whether it has an instrumental AND an acapella on a digital format
+    
+    Input:
+        group_ids - List of what.cd torrent group ids to check
+        api_handle - what.cd api handle, default=None which means make one
+    Output:
+        valid_group_ids - list of suitable group ids
+    '''
+    if api_handle is None:
+        api_handle = whatapi.WhatAPI(username=secret.username, password=secret.password)
+    # Store group IDs which have acapella/instrumental in different formats
+    valid_group_ids = []
+    # Check all provided group ids
+    for group_id in group_ids:
+        # Query API
+        time.sleep(2)
+        try:
+            response = api_handle.request('torrentgroup', id=group_id )
+        except:
+            print "An error occurred."
+            continue
+        # If there was an error, skip this group ID
+        if response['status'] != 'success':
+            print "An error occurred."
+            continue
+        # Check that a CD format has the opposite
+        for torrent in response['response']['torrents']:
+            # CD or WEB are both fine
+            if torrent['media'] == 'CD' or torrent['media'] == 'WEB':
+                # Want lossless torrents only
+                if torrent['encoding'] == 'Lossless':
+                    # If there's an instrumental
+                    if 'instrumental' in torrent['fileList'].lower():
+                        # Try all spellings again
+                        for spelling in ['capela', 'capella', 'cappela', 'cappella']:
+                            # Does this digital format have an acapella and the vinyl has the instrumental?
+                            if spelling in torrent['fileList'].lower():
+                                valid_group_ids += [group_id]
+                                print group_id
+    # Return all unique valid group IDs
+    return np.unique(valid_group_ids).tolist()
+
+# <codecell>
+
+acapella_ids = np.array( np.loadtxt( 'acapella_ids.txt' ), dtype=np.int )
+instrumental_ids = np.array( np.loadtxt( 'instrumental_ids.txt' ), dtype=np.int )
+has_both = np.intersect1d( acapella_ids, instrumental_ids )
+
+# <codecell>
+
+has_both = has_both_digital( has_both, api_handle=api_handle )
+
